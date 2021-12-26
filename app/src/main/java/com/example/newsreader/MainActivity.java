35c9b233a698d2929dd71a84d6d5f5efa295e344
Priyank Shah <48063358+iamspd@users.noreply.github.com>
2021-12-26 14:15:48 -0500

    MainActivity - articleContent fetched

--- app/src/main/java/com/example/newsreader/MainActivity.java
+++ app/src/main/java/com/example/newsreader/MainActivity.java
@@ -9,6 +9,10 @@ import android.util.Log;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
@@ -61,12 +65,69 @@ public class MainActivity extends AppCompatActivity {
                     result += current;
                     data = inputStreamReader.read();
                 }
-                Log.i("News", result);
+               /* Log.i("News", result);*/
 
-            } catch (IOException e) {
+                JSONArray newsIdJsonArray = new JSONArray(result);
+
+                int totalCount = 20;
+
+                if (newsIdJsonArray.length() < 20) {
+                    totalCount = newsIdJsonArray.length();
+                }
+
+                for (int i = 0; i < totalCount; i++) {
+                   // Log.i("ArrayItems", newsIdJsonArray.getString(i));
+
+                    String newsItemId = newsIdJsonArray.getString(i);
+
+                    url = new URL(" https://hacker-news.firebaseio.com/v0/item/" + newsItemId + ".json?print=pretty");
+                    httpURLConnection = (HttpURLConnection) url.openConnection();
+
+                    inputStream = httpURLConnection.getInputStream();
+                    inputStreamReader = new InputStreamReader(inputStream);
+
+                    int newsData = inputStreamReader.read();
+                    String newsDataItems = "";
+
+                    while (newsData != -1) {
+                        char currentNewsData = (char) newsData;
+                        newsDataItems += currentNewsData;
+                        newsData = inputStreamReader.read();
+                    }
+
+                    // Log.i("News", newsDataItems);
+
+                    JSONObject newsJsonObject = new JSONObject(newsDataItems);
+                    if (!newsJsonObject.isNull("title") && !newsJsonObject.isNull("url")) {
+                        String articleTitle = newsJsonObject.getString("title");
+                        String articleURL = newsJsonObject.getString("url");
+
+                        // Log.i("URLTitle", articleTitle + articleURL);
+
+                        url = new URL(articleURL);
+                        httpURLConnection = (HttpURLConnection) url.openConnection();
+
+                        inputStream = httpURLConnection.getInputStream();
+                        inputStreamReader = new InputStreamReader(inputStream);
+
+                        data = inputStreamReader.read();
+                        StringBuilder articleInfo = new StringBuilder();
+
+                        while (data != -1) {
+                            char currentArticleInfo = (char) data;
+                            articleInfo.append(currentArticleInfo);
+
+                            data = inputStreamReader.read();
+                        }
+
+                        Log.i("info", articleInfo.toString());
+                    }
+
+                }
+
+            } catch (IOException | JSONException e) {
                 e.printStackTrace();
             }
-
 
             return null;
         }
