1a0c415847b2d139734d262a575dd6d24b4cdad1
Priyank Shah <48063358+iamspd@users.noreply.github.com>
2021-12-26 14:49:41 -0500

    ArticleActivity - WebView added

--- app/src/main/java/com/example/newsreader/ArticleActivity.java
+++ app/src/main/java/com/example/newsreader/ArticleActivity.java
@@ -3,12 +3,22 @@ package com.example.newsreader;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
 
 public class ArticleActivity extends AppCompatActivity {
+
+    // widgets
+    private WebView articleWebView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_article);
+
+        articleWebView = findViewById(R.id.webView);
+
+        articleWebView.getSettings().setJavaScriptEnabled(true);
+        articleWebView.setWebViewClient(new WebViewClient());
     }
 }
